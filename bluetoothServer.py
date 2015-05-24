568c22ce7b5fbde09fcb859bfbf504977f3cc3c1
EvanFoo <efpepper22@gmail.com>
2015-05-24 06:44:32 -0700

    Bluetooth Server can now reveive images but it is slow

--- bluetoothServer.py
+++ bluetoothServer.py
@@ -11,16 +11,18 @@ server_sock.bind(("",port))
 server_sock.listen(1)
 print "listening on port %d" % port
 
+
 uuid = "00001101-0000-1000-8000-00805F9B34FB"
 
 def listen(input_client_sock):
 	while(1):
 		incomingData = "nothing"
 		incomingData = input_client_sock.recv(1024)
-		if incomingData != "nothing":
-			os.system('cls' if os.name == 'nt' else 'clear')
-			print incomingData
-	print input_client_sock
+		if incomingData != "nothing":		
+			f = open('image', 'a+')
+			f.write(incomingData)
+			f.close()
+			print("read the data")
 
 def write(client_sock, data):
 	client_sock.send(data)
